729183bb3eef8288b264d5b77eae5426f997d3da
EimearCunningham <eimearcunningham@hotmail.com>
2020-10-13 11:29:15 +0000

    About Us section basic functionality added

---
+++ assets/js/about-us.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+   $(".about-paragraph").hide();
+
+    $("#story-image").click(function(){
+  $("#story-paragraph").show();
+    });
+
+    $("#founders-image").click(function(){
+  $("#founders-paragraph").show();
+    });
+
+    $("#team-image").click(function(){
+  $("#team-paragraph").show();
+    });
+});
