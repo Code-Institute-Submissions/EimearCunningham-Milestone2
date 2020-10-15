9208581c4704b7b920eab1a8ea5317bc231da1c0
EimearCunningham <eimearcunningham@hotmail.com>
2020-10-15 12:39:47 +0000

    Google maps API enabled

---
+++ assets/js/maps.js
@@ -0,0 +1,11 @@
+var APIkey = "AIzaSyCIDwqcfYrqmFmO_FmuEJiFwIJZC9oXvkw";
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 9,
+        center: {
+            lat: 51.509865,
+            lng: -0.118092
+        }
+    });
+}
