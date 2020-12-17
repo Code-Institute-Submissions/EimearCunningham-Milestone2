9f3fb0569fc43fffbb4fba78c7b0aa671d2bda5b
EimearCunningham <eimearcunningham@hotmail.com>
2020-12-17 21:36:37 +0000

    InfoWindows added to Google Maps markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,6 +1,42 @@
 var APIkey = "AIzaSyCIDwqcfYrqmFmO_FmuEJiFwIJZC9oXvkw";
 
+
+
 function initMap() {
+
+    var westminster = 
+        {
+            info: '<strong>Westminster</strong>',
+            lat: 51.501903,
+            l0ng: -0.124273,
+        }
+    var stpauls =
+        {
+            info: '<strong>St. Pauls</strong>',
+            lat: 51.512883,
+            lng: -0.096657
+        }
+    var elephantandcastle=
+        {
+            info: '<strong>Elephant & Castle</strong>',
+            lat: 51.495489,
+            lng: -0.102219
+        }
+    var brixton=
+        {
+            info: '<strong>Brixton</strong>',
+            lat: 51.463402,
+            lng: -0.111467
+        }
+
+    var locations = [
+    [westminster.info, westminster.lat, westminster.lng, 0],
+    [stpauls.info, stpauls.lat, stpauls.lng, 1],
+    [elephantandcastle.info, elephantandcastle.lat, elephantandcastle.lng, 2],
+    [brixton.info, brixton.lat, brixton.lng, 3]
+  ]
+
+
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 12,
         center: {
@@ -9,27 +45,32 @@ function initMap() {
         }
     });
 
+    var infowindow = new google.maps.InfoWindow({});
 
-var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var locations = [
-    {lat: 51.501903, 
-    lng: -0.124273 },
-    {lat: 51.512883, 
-    lng: -0.096657 },
-    {lat: 51.495489, 
-    lng: -0.102219},
-    {lat: 51.463402, 
-    lng: -0.111467}
-];
+    var marker, i;
 
-var markers = locations.map(function(location,i){
-    return new google.maps.Marker({
-        position: location,
-        label: labels[i % labels.length]
-    });
-});
+    //var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map,
+        });
+
+        google.maps.event.addListener(
+            marker,
+            'click',
+            (function (marker, i) {
+                return function () {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                };
+            })(marker, i)
+        );
+
+        //var markerCluster = new MarkerClusterer(map, marker,
+          //  { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+    };
 }
